6ab8141f10aa93dd1da11fce254fbe7dae2df6fc
Matthew Garber <matgar1997@gmail.com>
2020-02-27 16:52:40 -0500

    easily load enviroment vars

--- manage.py
+++ manage.py
@@ -2,8 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutor_finder.settings.heroku')
     try:
         from django.core.management import execute_from_command_line
