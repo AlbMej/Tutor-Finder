4f62a735784eab4f9d91b1ddb48c9e2d993b7735
John Parent <parenj@rpi.edu>
2020-04-14 23:49:09 -0400

    email backend setup

--- tutor_finder/settings/settings.py
+++ tutor_finder/settings/settings.py
@@ -124,7 +124,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tutor.finder.team@gmail.com'
+EMAIL_HOST_PASSWORD = 'ZJkkunjKwfGZs47'
+
+#DEFAULT_FROM_EMAIL = 'donotreply@tutorfinder.com'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
