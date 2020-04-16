85969323e9840c2faf8e34f3b3a097f8012c773d
John Parent <parenj@rpi.edu>
2020-04-16 16:18:49 -0400

    email functional

--- tutor_finder/core/forms.py
+++ tutor_finder/core/forms.py
@@ -16,7 +16,16 @@ from django.core.exceptions import ValidationError
 from tutor_finder.core.models import UserInfo
 from tutor_finder.core.models import Tutor
 import datetime
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
 
 
 # Form to display text/other submission widgets,
